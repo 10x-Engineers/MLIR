20879e3e7123fd7fb1865056db024c8460b413b9
nouman-10x <noumanamir453@gmail.com>
2025-03-01 01:42:20 +0500

    Add AffineFullUnroll Pass using MLIR API - Added code in AffineFullUnroll.cpp/h - Added tests
    Signed-off-by: nouman-10x <noumanamir453@gmail.com>

--- tools/dummy-opt.cpp
+++ tools/dummy-opt.cpp
@@ -4,9 +4,13 @@
 #include "mlir/Pass/PassRegistry.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
+#include "lib/Transform/Affine/AffineFullUnroll.h"
+
 int main(int argc, char **argv) {
     mlir::DialectRegistry registry;
     mlir::registerAllDialects(registry);
+
+    mlir::PassRegistration<mlir::dummy::AffineFullUnrollPass>();
 
     return mlir::asMainReturnCode(
         mlir::MlirOptMain(argc, argv, "Dummy Pass Driver", registry)
