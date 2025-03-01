5e90cafcab037fb6db6ffbd5c8d6bc147d72a0c5
nouman-10x <noumanamir453@gmail.com>
2025-03-01 17:04:47 +0500

    Basic RewritePattern Pass - Added MulToAddPass
    Signed-off-by: nouman-10x <noumanamir453@gmail.com>

--- tools/dummy-opt.cpp
+++ tools/dummy-opt.cpp
@@ -5,12 +5,14 @@
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
 #include "lib/Transform/Affine/AffineFullUnroll.h"
+#include "lib/Transform/Arith/MulToAdd.h"
 
 int main(int argc, char **argv) {
     mlir::DialectRegistry registry;
     mlir::registerAllDialects(registry);
 
     mlir::PassRegistration<mlir::dummy::AffineFullUnrollPass>();
+    mlir::PassRegistration<mlir::dummy::MulToAddPass>();
 
     return mlir::asMainReturnCode(
         mlir::MlirOptMain(argc, argv, "Dummy Pass Driver", registry)
