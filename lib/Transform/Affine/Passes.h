8b5388b47342633dd3209d97a767b69b43cbe0d5
nouman-10x <noumanamir453@gmail.com>
2025-03-02 15:10:26 +0500

    Minor Fix
    Signed-off-by: nouman-10x <noumanamir453@gmail.com>

--- lib/Transform/Affine/Passes.h
+++ lib/Transform/Affine/Passes.h
@@ -9,7 +9,7 @@ namespace dummy {
 #define GEN_PASS_REGISTRATION
 #include "lib/Transform/Affine/Passes.h.inc"
 
-} // namespace tutorial
+} // namespace dummy
 } // namespace mlir
 
 #endif // LIB_TRANSFORM_AFFINE_PASSES_H_
--- tools/dummy-opt.cpp
+++ tools/dummy-opt.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
 
-  mlir::tutorial::registerAffinePasses();
+  mlir::dummy::registerAffinePasses();
   mlir::PassRegistration<mlir::dummy::MulToAddPass>();
 
   return mlir::asMainReturnCode(
